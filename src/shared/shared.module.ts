421a43c02076684030c81286aa680976926935e7
Xely <julien_guya@msn.com>
2020-07-28 23:10:13 +0200

    Ajout bootstrap Architecture de base du projet (core, shared etc) Layout de base / navbar

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule }                from '@angular/core';
+import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+    declarations: [],
+    imports     : [
+        CommonModule
+    ]
+})
+export class SharedModule {
+}
