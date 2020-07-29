2879df3803f0a70ee2d840e7ca221a8748f84a23
Thomas Sanceo <tsanceo@yahoo.com>
2020-07-29 11:27:51 +0200

    Revert " Files archi"
    This reverts commit 48d822fad917b80416af2cf771e0d53e859459e7.
    
    # Conflicts:
    #	angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppComponent }     from './app.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HomeComponent,
-        FooterComponent
+        AppComponent
     ],
     imports     : [
         BrowserModule,
--- src/app/settings.ts
+++
@@ -1,1 +0,0 @@
-export const APIURL = 'localhost:3000';
