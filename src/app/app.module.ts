886ab03b74eceb20efd2291def5c01a9dfa8bc07
Thomas Sanceo <tsanceo@yahoo.com>
2020-07-29 12:35:57 +0200

    fix: prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,18 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbNavModule }  from '@ng-bootstrap/ng-bootstrap';
-import { CoreModule }    from '../core/core.module';
-import { SharedModule }  from '../shared/shared.module';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent }     from './app.component';
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent,
-        HomeComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        CoreModule,
-        SharedModule,
-        NgbNavModule
-    ],
-    providers   : [],
-    bootstrap   : [AppComponent]
+    declarations: [AppComponent, NavbarComponent, HomeComponent],
+    imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule, NgbNavModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { NgModule }                from '@angular/core';
+import { NgModule } from '@angular/core';
 import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [],
-    imports     : [
-        CommonModule
-    ]
+    imports: [CommonModule],
 })
-export class SharedModule {
-}
+export class SharedModule {}
