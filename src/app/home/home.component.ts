22c533a13d82e5b6d02edd416d5b6a83fcb45ddd
Thomas Sanceo <tsanceo@yahoo.com>
2020-07-29 13:28:08 +0200

    core(#1): Fix lint

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     constructor() {}
 
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     ngOnInit(): void {}
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     constructor() {}
 
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     ngOnInit(): void {}
 }
