48d822fad917b80416af2cf771e0d53e859459e7
AKA Camille <makaye151@gmail.com>
2020-07-29 09:53:17 +0200

    Files archi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent }     from './app.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        FooterComponent
     ],
     imports     : [
         BrowserModule,
